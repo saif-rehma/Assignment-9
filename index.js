29b9568cfbfda1035fe2b142146e3e113f915b7c
saif-rehma <144496587+saif-rehma@users.noreply.github.com>
2023-09-23 16:18:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let faverateNumber = 6;
+let message = `My faverate number is ${faverateNumber}`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,8 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+let faverateNumber: number = 6
+
+let message: string = `My faverate number is ${faverateNumber}`
+
+console.log(message)
